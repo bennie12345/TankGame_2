917b2a71e29988f5cf1be7a1adcc3eda58024b88
bennie12345 <bmuijldert@gmail.com>
2015-01-19 19:37:48 +0100

    Tankgame helemaal af

---
+++ Assets/Scripts/EnemyRotateTurret.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyRotateTurret : BaseRotateTurret {
+    public Transform player;
+
+    override protected void Start()
+    {
+        
+        base.Start();
+    }
+	// Update is called once per frame
+	override protected void Update () {
+
+        if (player != null)
+        {
+            targetPos = player.position + Vector3.up * 1f;
+            base.Update();
+        }
+	}
+}
